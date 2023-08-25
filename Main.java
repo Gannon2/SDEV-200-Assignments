64781ae1e6fbbf7fa5be349c0a6aa6ab8ced6961
Gannon2 <111807284+Gannon2@users.noreply.github.com>
2023-08-24 22:21:21 -0400

    M01 Programming Assignment
    8/24/2023

---
+++ Main.java
@@ -0,0 +1,42 @@
+
+// Gannon Stephens
+// 8/24/2023
+// Program displays the table as shown in assignment 6.9
+// 
+public class Main {
+  /** Main Method */
+  public static void main(String[] args) {
+
+    // Adds the table
+    System.out.println(
+        "\nFeet     Meters     |     Meters     Feet\n" +
+            "---------------------------------------------");
+    // Creates the top part of the table, so that it looks just like the table shown in the Liang reading.
+    for (double feet = 1.0, meters = 20.0; feet <= 10.0; feet++, meters += 5) {
+      // Line 13 applies the conversion formula that was shown in the Liang reading, also shown below:
+      // The formula for the conversions are:
+      // meter = 0.305 * foot
+      // foot = 3.279 * meter
+      // QUESTION: I was only able to figure out printf because Replit will try to auto corrent code sometimes, how come printIn doesn't work with lines 21, 22, 26, qne 27? I was having trouble figuring that out for 2 hours.
+      System.out.printf("%4.1f     ", feet);
+      System.out.printf("%6.3f", footTOMeter(feet));
+      // ^ Returns the integers that are defined from lines 29 to 34
+      System.out.print("     |     ");
+      // ^ Places the middle line on the table, it only goes down because of the "/n"
+      System.out.printf("%-11.1f", meters);
+      System.out.printf("%7.3f\n", meterTOFoot(meters));
+      // ^ Returns the integers that are defined from lines 29 to 34
+
+    }
+  }
+
+  /** Converts f to m */
+  public static double footTOMeter(double foot) {
+    return 0.305 * foot;
+  }
+
+  /** Converts m to f */
+  public static double meterTOFoot(double meter) {
+    return 3.279 * meter;
+  }
+}
